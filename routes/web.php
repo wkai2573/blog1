bbcae8d16b12be4b22de6b2e2799df0efed04a77
wkai2573 <wkai2573@gmail.com>
2020-06-28 19:41:21 +0800

    day 15

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Post;
 use App\Subject;
 use Illuminate\Support\Facades\Route;
 
@@ -15,47 +16,56 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-	return view('welcome');
+  return view('welcome');
 });
+
+// View__________
 
 //導向view
 Route::get('/hello-world', function () {
-	return view('hello_world');
+  return view('hello_world');
 });
 
 //導向view 並傳參數
 Route::get('/about_us', function () {
-	return view('about_us', ['name'=> 'Laravel 範例']);
+  return view('about_us', ['name' => 'Laravel 範例']);
 });
 
+// Controller__________
 
-
-//交給 Controller 處理
+// 交給 Controller 處理
 Route::get('/inspire', 'inspireController@inspire');
 
-//測試新增
-Route::get('/add', function(){
-	// $newSub = new App\Subject;
-	// $newSub->name = 'BB';
-	// $newSub->save();
-	$p = new App\Post;
-	$p->content = 'AA-2';
-	$p->subject_id = '1';
-	$p->save();
+// 測試新增
+Route::get('/add', function () {
+  // $newSub = new App\Subject;
+  // $newSub->name = 'BB';
+  // $newSub->save();
+  $p = new Post;
+  $p->content = 'AA-2';
+  $p->subject_id = '1';
+  $p->save();
 });
 
-//測試存取
-Route::get('/test', function(){
-	// 取得所有post
-	// return App\Post::get();
+// 取得所有post
+Route::get('/getPost', function () {
+  return Post::get();
+});
+// 取得post，使用LazyCollection物件(取用時才抓資料)
+Route::get('/getPost2', function () {
+	return Post::cursor()->slice(10, 5);
+});
 
-	// 取得subject id1 的 posts
-	// $subject = Subject::find(1);
-	// $posts = $subject->posts;
-	// return $posts;
+// 取得subject id1 的 posts
+Route::get('/subject_1', function () {
+  $subject = Subject::find(1);
+  $posts = $subject->posts;
+  return $posts;
+});
 
-	// 取得tag id1 的 posts
-	$tag = App\Tag::find(1);
-	$posts = $tag->posts;
-	return $posts;
+// 取得tag id1 的 posts
+Route::get('/postsByTag1', function () {
+  $tag = App\Tag::find(1);
+  $posts = $tag->posts;
+  return $posts;
 });
